de71da552dbd84ec2881a5a7681432798f037cb0
mxl <874843506@qq.com>
2023-07-19 16:39:32 +0800

    测试提交到仓库区2

---
+++ thread_exer.py
@@ -0,0 +1,29 @@
+"""
+    自定义进程类
+"""
+import time
+from multiprocessing import Process
+
+
+class MyProcess(Process):
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    def func1(self):
+        time.sleep(3)
+        print("步骤1")
+
+    def func2(self):
+        time.sleep(3)
+        print("步骤2")
+
+    def run(self):
+        self.func1()
+        self.func2()
+
+
+if __name__ == '__main__':
+    p = MyProcess("3")
+    p.start()
+    p.join()
